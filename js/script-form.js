87c8f3152bdd5387b1944fe78239b3690f88a5af
Javier Navarrete <44924176+JavIgna@users.noreply.github.com>
2023-08-16 23:49:01 -0400

    add copyright and corrections on contact-form

--- js/script-form.js
+++ js/script-form.js
@@ -3,9 +3,54 @@ document
   .addEventListener("submit", function (event) {
     event.preventDefault();
 
+    // Validar los campos antes de enviar el formulario
+    const nameInput = document.getElementById("name");
+    const emailInput = document.getElementById("email");
+    const phoneInput = document.getElementById("phone");
+    const messageInput = document.getElementById("message");
+    let formIsValid = true;
+
+    if (nameInput.value.trim() === "") {
+      nameInput.classList.add("is-invalid");
+      formIsValid = false;
+    } else {
+      nameInput.classList.remove("is-invalid");
+    }
+
+    if (emailInput.value.trim() === "") {
+      emailInput.classList.add("is-invalid");
+      formIsValid = false;
+    } else if (!isValidEmail(emailInput.value.trim())) {
+      emailInput.classList.add("is-invalid");
+      emailInput.nextElementSibling.textContent =
+        "Correo electrónico invalido.";
+      formIsValid = false;
+    } else {
+      emailInput.classList.remove("is-invalid");
+      emailInput.nextElementSibling.textContent = "";
+    }
+
+    if (phoneInput.value.trim() === "") {
+      phoneInput.classList.add("is-invalid");
+      formIsValid = false;
+    } else {
+      phoneInput.classList.remove("is-invalid");
+    }
+
+    if (messageInput.value.trim() === "") {
+      messageInput.classList.add("is-invalid");
+      formIsValid = false;
+    } else {
+      messageInput.classList.remove("is-invalid");
+    }
+
+    if (!formIsValid) {
+      return;
+    }
+
     fetch("https://formspree.io/f/mdorerkj", {
       method: "POST",
-      mode: "no-cors", // Cambiar el modo a 'no-cors'
+      mode: "no-cors",
       body: new FormData(document.getElementById("contactForm")),
     })
       .then(() => {
@@ -18,4 +63,12 @@ document
           .getElementById("submitErrorMessage")
           .classList.remove("d-none");
       });
+
+    const submitButton = document.getElementById("submitButton");
+    submitButton.disabled = true;
   });
+
+function isValidEmail(email) {
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailPattern.test(email);
+}
